03b06d512df5baf92cc3c157ef7c05223042675d
Olesya <olees.orlenko@yandex.ru>
2024-02-03 14:15:36 +0300

    Update settings.py

--- file_uploading/file_uploading/settings.py
+++ file_uploading/file_uploading/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "file_uploading.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
